ea2627cc5888e4b54abd3561af399cdff6fc9e3a
SangTran <roblox2016rb@gmail.com>
2023-11-16 03:31:15 +0700

    Re-work with Menu
    Temp Menu added to work perfectly with login and register

---
+++ Assets/Scripts/MenuLoginRegister/Menu.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Button Play;
+    public Button Opion;
+    public Button Exit; 
+    void Start()
+    {
+        Play.onClick.AddListener(MovetoGame);
+        // Opion.onClick.AddListener(MovetoGame);
+        // Exit.onClick.AddListener(MovetoGame);
+    }
+    void MovetoGame(){
+        SceneManager.LoadScene("Quyet");
+    }
+    }
+
